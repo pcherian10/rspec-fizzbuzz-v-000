87b699b5aaf06c63f046c313e790fc6192f02493
Paul <pcherian10@gmail.com>
2018-04-23 01:47:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,7 @@
 def fizzbuzz (n)
   
-  if(n % 3) == 0
+  if(n % 3 == 0 && n % 5 != 0) 
     return "Fizz"
-  elsif(n % 5) == 0
-    return "Buzz"
-  end
   
-  if(n % 3 == 0 && n % 5 == 0)
-    return "FizzBuzz"
-  else
-    return nil
-  end
  
 end
